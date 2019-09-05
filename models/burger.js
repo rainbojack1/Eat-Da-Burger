1a10ad389fab994035e61cfb522025f0be53adf8
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-09-04 21:05:52 -0400

    Set up the functions to add a burger.

--- models/burger.js
+++ models/burger.js
@@ -6,14 +6,13 @@ var burger = {
           cb(res);
         });
       },
-      // The variables cols and vals are arrays.
-      /*create: function(cols, vals, cb) {
-        orm.create("cats", cols, vals, function(res) {
+      create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
           cb(res);
         });
       },
-      update: function(objColVals, condition, cb) {
-        orm.update("cats", objColVals, condition, function(res) {
+      /*update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
           cb(res);
         });
       }*/
